e94046394036ac6860d53b85295ef49e76af644a
jdavidg1 <jdavidg_1@hotmail.com>
2019-10-01 18:04:02 -0500

    Versión 1. Conexión básica

--- FactuxD/Form1.cs
+++ FactuxD/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;        //Permite utilizar las variables de conexión
 
 namespace FactuxD
 {
@@ -15,6 +16,23 @@ namespace FactuxD
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection Con = new SqlConnection("Data Source=LAPTOP-9VN6QT9R\\SQLEXPRESS;Initial Catalog=Administracion;Integrated Security=True");
+                Con.Open();
+                MessageBox.Show("La conexión fue exitosa");
+            }
+            catch(Exception Error)
+            {
+
+                MessageBox.Show("Se ha producido un error: " + Error.Message);
+
+            }
+
         }
     }
 }
